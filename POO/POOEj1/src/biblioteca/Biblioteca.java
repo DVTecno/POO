f870c273b0237d7ac9248368a4e63df6b57a0862
Diego  Veras <verasdiegoca78@gmail.com>
2023-03-28 17:19:33 -0300

    Update Biblioteca.java

--- POO/POOEj1/src/biblioteca/Biblioteca.java
+++ POO/POOEj1/src/biblioteca/Biblioteca.java
@@ -4,17 +4,17 @@ import Entidad.Libro;
 import java.util.Scanner;
 
 /**
- * "Ficciones" de Jorge Luis Borges. ISBN: 9789877380874. 150 p·ginas. "El
- * Aleph" de Jorge Luis Borges. ISBN: 9789500723129. 224 p·ginas. "El T˙nel" de
- * Ernesto Sabato. ISBN: 9788432228313. 168 p·ginas. "Rayuela" de Julio
- * Cort·zar. ISBN: 9788466320711. 736 p·ginas.
+ * "Ficciones" de Jorge Luis Borges. ISBN: 9789877380874. 150 p√°ginas. "El
+ * Aleph" de Jorge Luis Borges. ISBN: 9789500723129. 224 p√°ginas. "El T√∫nel" de
+ * Ernesto Sabato. ISBN: 9788432228313. 168 p√°ginas. "Rayuela" de Julio
+ * Cort√°zar. ISBN: 9788466320711. 736 p√°ginas.
  *
  * @author veras
  */
 public class Biblioteca {
 
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
+       
         Libro libro1 = new Libro();
 
         libro1.cargarLibro();
