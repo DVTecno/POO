d621f0e46004dca7b23633fc045a3977364ce2ac
veras <verasdiegoca78@gmail.com>
2023-03-28 17:00:35 -0300

    Subiendo Ejercicios 1

--- POO/POOEj1/src/biblioteca/Biblioteca.java
+++ POO/POOEj1/src/biblioteca/Biblioteca.java
@@ -15,7 +15,10 @@ public class Biblioteca {
     public static void main(String[] args) {
 
         Libro libro1 = new Libro();
+        
+        
         libro1.cargarLibro();
         libro1.mostrarLibro();
+        System.out.println("Hola");
     }
 }
