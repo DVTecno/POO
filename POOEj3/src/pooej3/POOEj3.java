26497323439b27b3945398a7627b97beb2fe2dbb
veras <verasdiegoca78@gmail.com>
2023-03-28 12:21:11 -0300

    Sincro repo com poo

---
+++ POOEj3/src/pooej3/POOEj3.java
@@ -0,0 +1,10 @@
+package pooej3;
+
+public class POOEj3 {
+
+    public static void main(String[] args) {
+     
+
+    }
+
+}
---
+++ POOEj4/src/pooej4/POOEj4.java
@@ -0,0 +1,10 @@
+package pooej4;
+
+public class POOEj4 {
+
+    public static void main(String[] args) {
+     
+
+    }
+
+}
