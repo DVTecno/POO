8bfb629344ef433df6aac64dd93065c0874d84b9
veras <verasdiegoca78@gmail.com>
2023-03-28 16:06:25 -0300

    Sincro repo com poo

---
+++ POO/POOEj2/src/Circunferencia/Circunferencia.java
@@ -0,0 +1,27 @@
+package Circunferencia;
+
+public class Circunferencia {
+
+    private double radio;
+
+    public Circunferencia(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+}
--- POO/POOEj2/src/pooej2/POOEj2.java
+++ POO/POOEj2/src/pooej2/POOEj2.java
@@ -1,11 +1,21 @@
 package pooej2;
 
-import java.util.Scanner;
+import Circunferencia.Circunferencia;
 
 public class POOEj2 {
 
     public static void main(String[] args) {
-     Scanner leer = new Scanner(System.in);
+        Circunferencia circunferencia = new Circunferencia(2.0);
+        System.out.println("El radio de la circunferencia es: " + circunferencia.getRadio());
+        System.out.println("El area de la circunferencia es: "+circunferencia.calcularArea());
+        System.out.println("El perimetro de la circunferencia es: "+circunferencia.calcularPerimetro());
+        System.out.println("-------------------------------------------------------------------");
+        circunferencia.setRadio(9.0);
+        System.out.println("Nuevo radio: "+circunferencia.getRadio());
+        System.out.println("El area de la circunferencia actulizada es: "+circunferencia.calcularArea());
+        System.out.println("El perimetro de la circunferencia actulizada es: "+circunferencia.calcularPerimetro());
+        
+        
 
     }
 
