9b8614df178fe01b7964f71695ecb84b0662d183
veras <verasdiegoca78@gmail.com>
2023-04-03 10:29:09 -0300

    Subiendo Ejercicios Automoviles

---
+++ POO/automovil/src/main/java/com/dvtecno/automovil/Automovil.java
@@ -0,0 +1,15 @@
+
+package com.dvtecno.automovil;
+
+import com.dvtecno.automovil.igu.Principal;
+
+
+
+public class Automovil {
+
+    public static void main(String[] args) {
+        Principal capaPrincipal = new Principal();
+        capaPrincipal.setVisible(true);
+        capaPrincipal.setLocationRelativeTo(null);
+    }
+}
