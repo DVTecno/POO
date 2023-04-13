a0d7d3f7eee533c0894d29b3bfa2a66b8d19225d
veras <verasdiegoca78@gmail.com>
2023-04-13 14:11:11 -0300

    peluqueria caniana, proyecto de todocode.

---
+++ POO/Peluqueria/src/main/java/com/dvtecno/peluqueria/Peluqueria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dvtecno.peluqueria;
+
+/**
+ *
+ * @author veras
+ */
+public class Peluqueria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ POO/Peluqueria/src/main/java/igu/CargarDatos.java
@@ -0,0 +1,32 @@
+package igu;
+
+public class CargarDatos extends javax.swing.JFrame {
+
+    public CargarDatos() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
