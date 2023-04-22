e16b0c6395146edfcdd5ee47d190bb44d1a5245d
veras <verasdiegoca78@gmail.com>
2023-04-22 20:34:38 -0300

    realizando cambios

--- POO/POOGuia9ExtrasEj2/src/Servicio/AhorcadoService.java
+++ POO/POOGuia9ExtrasEj2/src/Servicio/AhorcadoService.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class AhorcadoService {
 
-    Ahorcado ahorcado = new Ahorcado();
+    
     Scanner leer = new Scanner(System.in).useDelimiter("\n");
 //Metodo crearJuego(): le pide la palabra al usuario y cantidad de jugadas máxima. 
 //Con la palabra del usuario, pone la longitud de la palabra, como la longitud del vector.
@@ -13,13 +13,13 @@ public class AhorcadoService {
 //la palabra en un índice del vector. Y también, guarda la cantidad de jugadas máximas 
 //y el valor que ingresó el usuario.
 
-    public void crearJuego() {
+    public void crearJuego(Ahorcado ahorcado) {
 
         System.out.println("Definas las oportunidades: ");
         ahorcado.setCantidadJugadasMaximas(leer.nextInt());
         System.out.println("Ingrese la palabra: ");
         String palabra = leer.next();
-      
+
         String[] vectorPalabra = new String[palabra.length()];
         String[] vectorPalabraAux = new String[palabra.length()];
         for (int i = 0; i < vectorPalabra.length; i++) {
@@ -34,13 +34,13 @@ public class AhorcadoService {
 //Método longitud(): muestra la longitud de la palabra que se debe encontrar. Nota: buscar 
 //como se usa el vector.length.   
 
-    public void longitud() {
+    public void longitud(Ahorcado ahorcado) {
         System.out.println("El numero de letras a buscar es: " + ahorcado.getPalabra().length);
     }
 //Método buscar(letra):  este método recibe una letra dada por el usuario y busca si la letra 
 //ingresada es parte de la palabra o no. También informará si la letra estaba o no.  
 
-    public boolean buscar(String letra) {
+    public boolean buscar(Ahorcado ahorcado, String letra) {
         boolean bandera = false;
         int count = 0;
         for (int i = 0; i < ahorcado.getPalabra().length; i++) {
@@ -63,29 +63,28 @@ public class AhorcadoService {
 //si la letra estaba y false si la letra no estaba, ya que, cada vez que se busque una letra 
 //que no esté, se le restará uno a sus oportunidades.
 
-    public String encontradas(String letra) {
+    public String encontradas(Ahorcado ahorcado, String letra) {
         boolean bandera = false;
         int count = 0;
         for (int i = 0; i < ahorcado.getPalabra().length; i++) {
             if (ahorcado.getPalabra()[i].equalsIgnoreCase(letra)) {
                 if (ahorcado.getPalabraAux()[i].equals("_")) {
-                    
-                    
+
                     ahorcado.getPalabraAux()[i] = letra;
                     count++;
-                    ahorcado.setCantidadLetrasEncontradas(ahorcado.getCantidadLetrasEncontradas()+count);
+                    ahorcado.setCantidadLetrasEncontradas(ahorcado.getCantidadLetrasEncontradas() + count);
                 }
                 bandera = true;
             }
         }
         if (!bandera) {
-            ahorcado.setCantidadJugadasMaximas(ahorcado.getCantidadJugadasMaximas()-1);
+            ahorcado.setCantidadJugadasMaximas(ahorcado.getCantidadJugadasMaximas() - 1);
         }
         return letra;
     }
 //Método intentos(): para mostrar cuántas oportunidades le queda al jugador.    
 
-    public void intentos() {
+    public void intentos(Ahorcado ahorcado) {
         System.out.println("Te quedan: " + ahorcado.getCantidadJugadasMaximas() + " intentos");
     }
     //Método juego(): el método juego se encargará de llamar todos los métodos 
@@ -93,31 +92,30 @@ public class AhorcadoService {
 //o se quede sin intentos. Este método se llamará en el main. 
 
     public void juego() {
-        crearJuego();
+        Ahorcado ahorcado = new Ahorcado();
+        crearJuego(ahorcado);
         String letra;
-         boolean ganaste = false;
+        boolean ganaste = false;
+        longitud(ahorcado);
         do {
-            ahorcado();
-            intentos();
-            longitud();
+            ahorcado(ahorcado);
+            intentos(ahorcado);
             System.out.print("Ingresa la letra: ");
             letra = leer.next();
-            encontradas(letra);
-            System.out.println(ahorcado.getCantidadLetrasEncontradas());
-            System.out.println(ahorcado.getPalabra().length);
-            ganaste =ahorcado.getCantidadLetrasEncontradas() == ahorcado.getPalabra().length;
+            encontradas(ahorcado,letra);
+     
+            ganaste = ahorcado.getCantidadLetrasEncontradas() == ahorcado.getPalabra().length;
         } while (ahorcado.getCantidadJugadasMaximas() != 0 && ahorcado.getCantidadLetrasEncontradas() != ahorcado.getPalabra().length);
         if (ganaste) {
             System.out.println("Ganaste!");
-            
 
         } else {
             System.out.println("No encontraste la palabra ):");
         }
-        ahorcado();
+        ahorcado( ahorcado);
     }
 
-    private void ahorcado() {
+    private void ahorcado(Ahorcado ahorcado) {
         System.out.print("Palabra ");
         for (String letra : ahorcado.getPalabraAux()) {
             System.out.print(letra + " ");
