d4dbf05b9883b0277ab4aaf1f01cee35ade8157f
veras <verasdiegoca78@gmail.com>
2023-04-24 11:32:40 -0300

    realizando cambios

--- POO/POOGuia(ExtrasEj1/src/pooguia/extrasej1/POOGuiaExtrasEj1.java
+++ POO/POOGuia(ExtrasEj1/src/pooguia/extrasej1/POOGuiaExtrasEj1.java
@@ -1,3 +1,7 @@
+package pooguia.extrasej1;
+
+import java.util.Scanner;
+
 /*
 Crea una clase en Java donde declares una variable de tipo array de Strings que contenga los 
 doce meses del año, en minúsculas. A continuación, declara una variable mesSecreto de tipo String, 
@@ -9,40 +13,30 @@ No ha acertado. Intente adivinarlo introduciendo otro mes: agosto
 ¡Ha acertado!
 
  */
-package pooguia.extrasej1;
-
-import java.util.Scanner;
-
-/**
- *
- * @author CASA
- */
 public class POOGuiaExtrasEj1 {
 
     public static void main(String[] args) {
-
         Scanner leer = new Scanner(System.in);
         String[] meses = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
 
         String mesSecreto = meses[(int) (Math.random() * 12)];
-        char respuesta= ' ';
-        
+        char respuesta = ' ';
+
         do {
             System.out.println("Ingrese el mes secreto:");
             String adivine = leer.next();
 
-            if (
-                    adivine.equals(mesSecreto)) {
+            if (adivine.equals(mesSecreto)) {
 
-                System.out.println("Adivinaste!!");
+                System.out.println("Adivinaste!");
                 break;
-            }else{
+            } else {
                 System.out.println("No adivinaste!! intenta de nuevo");
                 System.out.println("Queres volver a intentar?");
                 respuesta = leer.next().charAt(0);
             }
-        }while(respuesta =='s');
-         System.out.println("El mes secreto: "+mesSecreto);   
+        } while (respuesta == 's');
+        System.out.println("El mes secreto: " + mesSecreto);
     }
 
 }
